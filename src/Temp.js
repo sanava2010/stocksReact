44f517ee6fa7c36116a2c16a3ac95c3fb8189d3f
sanava2010 <sanava2010@gmail.com>
2020-06-22 15:45:41 -0500

    checking

---
+++ src/Temp.js
@@ -0,0 +1,6 @@
+const arr=['hello', 'hi', 'how are you']
+const result=arr.filter(item => {return item.length>1})
+//console.log(result)
+let str= 'hello how are you?'
+const r2=str.split('').reverse().join('')
+console.log(r2)
